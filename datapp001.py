6631f7358e0b408d276fa161fe76ce180357345b
wicebing <wicebing@gmail.com>
2021-10-26 18:40:42 +0800

    5

--- datapp001.py
+++ datapp001.py
@@ -3,12 +3,40 @@ import os, glob
 import pandas as pd
 
 
+# db_file_path = '../datahouse/mimic-iv-0.4'
+
+# filepath = os.path.join(db_file_path, 'hosp')
+
+# filelist = glob.glob(os.path.join(filepath,'*'))
+
+# data = []
+# for fn in filelist:
+#     data.append(pd.read_csv(fn,sep=','))
+    
+# db_file_path = '../datahouse/mimic-iv-ed-1.0'
+# filepath = os.path.join(db_file_path, 'ed')
+
+# filelist = glob.glob(os.path.join(filepath,'*'))
+
+# data2 = []
+# for fn in filelist:
+#     data2.append(pd.read_csv(fn,sep=','))
+
+# import pickle
+# db_file_path = '../datahouse/ntuh_data_20201120_new.pickle'
+
+# data = pd.read_pickle(db_file_path)
+
+
 db_file_path = '../datahouse/mimic-iv-0.4'
 
-filepath = os.path.join(db_file_path, 'hosp')
+filepath = os.path.join(db_file_path, 'hosp', 'd_icd_diagnoses.csv')
 
-filelist = glob.glob(os.path.join(filepath,'*'))
 
-data = []
-for fn in filelist:
-    data.append(pd.read_csv(fn,sep=','))
+d_icd_diagnoses = pd.read_csv(filepath)
+
+arrest_icds = d_icd_diagnoses[d_icd_diagnoses['long_title'].str.contains("arrest|Arrest", na=False)]
+
+savfilepath = os.path.join(db_file_path, 'data_EDis', 'b_arrest_icds.csv')
+# arrest_icds.to_csv(savfilepath)
+
---
+++ datapp002.py
@@ -0,0 +1,75 @@
+import os, glob, datetime
+
+import pandas as pd
+
+db_file_path = '../datahouse/mimic-iv-0.4'
+
+
+
+filepath = os.path.join(db_file_path, 'data_EDis', 'b_arrest_icds.csv')
+arrest_icds = pd.read_csv(filepath)
+arrest_icds['select'] = 1
+
+# cardiac arrest/ > 18yrs, drop trauma/ sicu
+
+filepath = os.path.join(db_file_path, 'hosp', 'diagnoses_icd.csv')
+diagnoses_icd = pd.read_csv(filepath)
+
+
+diagnoses_icd_arrest = diagnoses_icd.merge(arrest_icds,how='inner',on='icd_code')
+
+sample = diagnoses_icd_arrest.iloc[0]
+
+subject_id = sample['subject_id']
+hadm_id = sample['hadm_id']
+
+
+filepath = os.path.join(db_file_path, 'hosp', 'labevents.csv')
+labevents = pd.read_csv(filepath)
+
+filepath = os.path.join(db_file_path, 'icu', 'icustays.csv')
+icustays = pd.read_csv(filepath)
+
+filepath = os.path.join(db_file_path, 'icu', 'inputevents.csv')
+inputevents = pd.read_csv(filepath)
+inputevents.loc[:,['starttime']] = pd.to_datetime(inputevents['starttime'])
+inputevents = inputevents.dropna(axis=0,subset=['totalamount'])
+
+filepath = os.path.join(db_file_path, 'icu', 'outputevents.csv')
+outputevents = pd.read_csv(filepath)
+outputevents.loc[:,['charttime']] = pd.to_datetime(outputevents['charttime'])
+
+# study group patient list is this icustays_select
+icustays_select = icustays[(icustays['first_careunit']=='Medical Intensive Care Unit (MICU)') |
+                           (icustays['first_careunit']=='Cardiac Vascular Intensive Care Unit (CVICU)') |
+                           (icustays['first_careunit']=='Coronary Care Unit (CCU)')
+                           ]
+
+icustays_select.loc[:,['intime']] = pd.to_datetime(icustays_select['intime'])
+icustays_select.loc[:,['outtime']] = pd.to_datetime(icustays_select['outtime'])
+icustays_select.loc[:,['io_24']] = 0. 
+icustays_select.loc[:,['i_24']] = 0. 
+icustays_select.loc[:,['o_24']] = 0. 
+
+# merge the IO events
+for i,sample in enumerate(icustays_select):
+    subject_id = sample['subject_id']
+    hadm_id = sample['hadm_id']
+    stay_id = sample['stay_id']
+    intime = sample['intime']
+    
+    inputevents_temp = inputevents[inputevents['stay_id']==stay_id]
+    temp = (inputevents_temp['starttime'] - intime) < datetime.timedelta(minutes=1440)
+    inputevents_temp_24 = inputevents_temp[temp]
+    inputamount24 = inputevents_temp_24['totalamount'].sum()
+
+    outputevents_temp = outputevents[outputevents['stay_id']==stay_id]
+    temp = (outputevents_temp['charttime'] - intime) < datetime.timedelta(minutes=1440)
+    outputevents_temp_24 = outputevents_temp[temp]
+    outputamount24 = outputevents_temp_24['value'].sum()
+    
+    idx = icustays_select[icustays_select['stay_id']==stay_id].index
+    
+    icustays_select.loc[idx,['io_24','i_24','o_24']] = inputamount24-outputamount24,inputamount24,outputamount24
+    
+    
