db5651e61b9ae4fdf52373348f3bbded348c8c2f
wicebing <wicebing@gmail.com>
2021-10-26 20:47:56 +0800

    5

--- datapp002.py
+++ datapp002.py
@@ -5,77 +5,86 @@ import pandas as pd
 db_file_path = '../datahouse/mimic-iv-0.4'
 
 
+# # step 1: drop trauma/ sicu
 
-filepath = os.path.join(db_file_path, 'data_EDis', 'b_arrest_icds.csv')
-arrest_icds = pd.read_csv(filepath)
-arrest_icds['select'] = 1
+# filepath = os.path.join(db_file_path, 'icu', 'icustays.csv')
+# icustays = pd.read_csv(filepath)
 
-# cardiac arrest/ > 18yrs, drop trauma/ sicu
+# filepath = os.path.join(db_file_path, 'icu', 'inputevents.csv')
+# inputevents = pd.read_csv(filepath)
+# inputevents.loc[:,['starttime']] = pd.to_datetime(inputevents['starttime'])
+# inputevents = inputevents.dropna(axis=0,subset=['totalamount'])
 
-filepath = os.path.join(db_file_path, 'hosp', 'diagnoses_icd.csv')
-diagnoses_icd = pd.read_csv(filepath)
+# temp = inputevents[(inputevents['itemid']==226361)|
+#                     (inputevents['itemid']==222168)|
+#                     (inputevents['itemid']==225943)
+#                     ]
+# inputevents = inputevents.drop(temp.index)
 
+# temp = inputevents[(inputevents['statusdescription']=='Stopped')|
+#                     (inputevents['itemid']=='Paused')
+#                     ]
+# inputevents = inputevents.drop(temp.index)
 
-diagnoses_icd_arrest = diagnoses_icd.merge(arrest_icds,how='inner',on='icd_code')
+# filepath = os.path.join(db_file_path, 'icu', 'outputevents.csv')
+# outputevents = pd.read_csv(filepath)
+# outputevents.loc[:,['charttime']] = pd.to_datetime(outputevents['charttime'])
 
-sample = diagnoses_icd_arrest.iloc[0]
+# temp = outputevents[(outputevents['itemid']==227489)|
+#                     (outputevents['itemid']==227488)|
+#                     (outputevents['itemid']==226633)
+#                     ]
+# outputevents = outputevents.drop(temp.index)
 
-subject_id = sample['subject_id']
-hadm_id = sample['hadm_id']
+# filepath = os.path.join(db_file_path, 'icu', 'd_items.csv')
+# d_items = pd.read_csv(filepath)
 
-
-filepath = os.path.join(db_file_path, 'hosp', 'labevents.csv')
-labevents = pd.read_csv(filepath)
-
-filepath = os.path.join(db_file_path, 'icu', 'icustays.csv')
-icustays = pd.read_csv(filepath)
-
-filepath = os.path.join(db_file_path, 'icu', 'inputevents.csv')
-inputevents = pd.read_csv(filepath)
-inputevents.loc[:,['starttime']] = pd.to_datetime(inputevents['starttime'])
-inputevents = inputevents.dropna(axis=0,subset=['totalamount'])
-
-filepath = os.path.join(db_file_path, 'icu', 'outputevents.csv')
-outputevents = pd.read_csv(filepath)
-outputevents.loc[:,['charttime']] = pd.to_datetime(outputevents['charttime'])
-
-# study group patient list is this icustays_select
-icustays_select = icustays[(icustays['first_careunit']=='Medical Intensive Care Unit (MICU)') |
-                           (icustays['first_careunit']=='Cardiac Vascular Intensive Care Unit (CVICU)') |
-                           (icustays['first_careunit']=='Coronary Care Unit (CCU)')
-                           ]
-
-icustays_select.loc[:,['intime']] = pd.to_datetime(icustays_select['intime'])
-icustays_select.loc[:,['outtime']] = pd.to_datetime(icustays_select['outtime'])
-icustays_select.loc[:,['io_24']] = 0. 
-icustays_select.loc[:,['i_24']] = 0. 
-icustays_select.loc[:,['o_24']] = 0. 
+# d_items_temp = d_items[['itemid', 'label']]
 
 
-length = len(icustays_select)
-# merge the IO events
-for i in tqdm.tqdm(range(length)):
+
+# # study group patient list is this icustays_select
+# icustays_select = icustays[(icustays['first_careunit']=='Medical Intensive Care Unit (MICU)') |
+#                             (icustays['first_careunit']=='Cardiac Vascular Intensive Care Unit (CVICU)') |
+#                             (icustays['first_careunit']=='Coronary Care Unit (CCU)')
+#                             ]
+
+# icustays_select.loc[:,['intime']] = pd.to_datetime(icustays_select['intime'])
+# icustays_select.loc[:,['outtime']] = pd.to_datetime(icustays_select['outtime'])
+# icustays_select.loc[:,['io_24']] = 0. 
+# icustays_select.loc[:,['i_24']] = 0. 
+# icustays_select.loc[:,['o_24']] = 0. 
+
+# # step 2: add io amount
+
+# length = len(icustays_select)
+# # merge the IO events
+# for i in tqdm.tqdm(range(length)):
     
-    sample = icustays_select.iloc[i]
+#     sample = icustays_select.iloc[i]
     
-    subject_id = sample['subject_id']
-    hadm_id = sample['hadm_id']
-    stay_id = sample['stay_id']
-    intime = sample['intime']
+#     subject_id = sample['subject_id']
+#     hadm_id = sample['hadm_id']
+#     stay_id = sample['stay_id']
+#     intime = sample['intime']
     
-    inputevents_temp = inputevents[inputevents['stay_id']==stay_id]
-    temp = (inputevents_temp['starttime'] - intime) < datetime.timedelta(minutes=1440)
-    inputevents_temp_24 = inputevents_temp[temp]
-    inputamount24 = inputevents_temp_24['totalamount'].sum()
+#     inputevents_temp = inputevents[inputevents['stay_id']==stay_id]
+#     temp = (inputevents_temp['starttime'] - intime) < datetime.timedelta(minutes=1440)
+#     inputevents_temp_24 = inputevents_temp[temp]
+#     inputamount24 = inputevents_temp_24['totalamount'].sum()
 
-    outputevents_temp = outputevents[outputevents['stay_id']==stay_id]
-    temp = (outputevents_temp['charttime'] - intime) < datetime.timedelta(minutes=1440)
-    outputevents_temp_24 = outputevents_temp[temp]
-    outputamount24 = outputevents_temp_24['value'].sum()
+#     outputevents_temp = outputevents[outputevents['stay_id']==stay_id]
+#     temp = (outputevents_temp['charttime'] - intime) < datetime.timedelta(minutes=1440)
+#     outputevents_temp_24 = outputevents_temp[temp]
+#     outputamount24 = outputevents_temp_24['value'].sum()
     
-    idx = icustays_select[icustays_select['stay_id']==stay_id].index
+#     idx = icustays_select[icustays_select['stay_id']==stay_id].index
     
-    icustays_select.loc[idx,['io_24','i_24','o_24']] = inputamount24-outputamount24,inputamount24,outputamount24
+#     icustays_select.loc[idx,['io_24','i_24','o_24']] = inputamount24-outputamount24,inputamount24,outputamount24
 
+# filepath = os.path.join(db_file_path, 'data_EDis', 'select_temp0.pdpkl')
+# icustays_select.to_pickle(filepath)
+
+# step 3: add io amount
 filepath = os.path.join(db_file_path, 'data_EDis', 'select_temp0.pdpkl')
-icustays_select.to_pickle(filepath)
+icustays_select = pd.read_pickle(filepath)
