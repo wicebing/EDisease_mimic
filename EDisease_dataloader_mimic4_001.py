5c4c971c0d91ed6a5e0b1b7131aa41d4597c5c9d
wicebing <wicebing@gmail.com>
2021-10-28 11:59:34 +0800

    5

---
+++ EDisease_dataloader_mimic4_001.py
@@ -0,0 +1,262 @@
+import pandas as pd
+import numpy as np
+import glob
+import os
+import random
+import datetime
+import torch
+import torch.nn as nn
+from torch.utils.data import Dataset, DataLoader
+
+from transformers import BertModel, BertTokenizer
+import torch
+
+# === impoert BERT ===
+BERT_tokenizer = BertTokenizer.from_pretrained("bert-base-multilingual-cased")
+#BERT_model = BertModel.from_pretrained("bert-base-multilingual-cased")
+# ====================
+
+class mimic_Dataset(Dataset):
+    def __init__(self, 
+                 ds,
+                 tokanizer,
+                 normalization,
+                 dsidx=None):
+        self.ds = ds
+        self.tokanizer = tokanizer
+        self.normalization = normalization
+        self.dsidx = dsidx
+        
+        if dsidx is None:
+            self.len = len(ds)
+        else:
+            self.len = len(dsidx)
+    
+    def __getitem__(self, index):
+        if self.dsidx is None:
+            e_patient = self.ds.iloc[index]
+        else: 
+            e_patient = self.ds.iloc[self.dsidx.iloc[index]]        
+       
+        chief_complaint = e_patient['CHIEFCOMPLAIN']
+        cc_tokens = self.tokanizer.tokenize(str(chief_complaint))
+        # add BERT cls head
+        cc_tokens = [self.tokanizer.cls_token, *cc_tokens]
+        cc_tokens = cc_tokens[:512]
+        cc_token_ids = self.tokanizer.convert_tokens_to_ids(cc_tokens)
+
+        H_select = ['EDDIAL', 'DIABETES', 'DEMENTIA', 'MIHX', 'DVT',
+            'CANCER', 'ETOHAB', 'ALZHD', 'ASTHMA', 'CEBVD', 'CKD', 'COPD',
+            'CHF', 'CAD', 'DEPRN', 'DIABTYP1', 'DIABTYP2', 'DIABTYP0', 'ESRD',
+            'HPE', 'EDHIV', 'HYPLIPID', 'HTN', 'OBESITY', 'OSA', 'OSTPRSIS','SUBSTAB',]
+
+        H_content = ['Dialysis, HD, PD, H/D, P/D, ',
+                     'Diabetes DM, ',
+                     'Dementia, ',
+                     'myocardial infarction (MI) (CAD), ',
+                     'deep vein thrombosis (DVT), ',
+                     'Cancer, ', 
+                     'Alcoholism, Alcohol misuse, abuse, dependence, ', 
+                     'Alzheimer’s disease, dementia, ',
+                     'Asthma, ', 
+                     'Cerebrovascular disease, stroke (CVA), transient ischemic attack (TIA), ', 
+                     'Chronic kidney disease (CKD), ', 
+                     'Chronic obstructive pulmonary disease (COPD), ',
+                     'Congestive heart failure, CHF, ', 
+                     'Coronary artery disease (CAD), ischemic heart disease (IHD), myocardial infarction (MI), ', 
+                     'Depression, ', 
+                     'Diabetes mellitus (DM) Type I',
+                     'Diabetes mellitus (DM) Type 2', 
+                     'Diabetes mellitus (DM)', 
+                     'End-stage renal disease (ESRD), ',
+                     'Pulmonary embolism (PE), deep vein thrombosis (DVT), venous thromboembolism (VTE), ', 
+                     'HIV infection, AIDS',
+                     'Hyperlipidemia, ', 
+                     'Hypertension, ', 
+                     'Obesity, ',
+                     'Obstructive sleep apnea (OSA), ', 
+                     'Osteoporosis, ',
+                     'Substance abuse or dependence, ',]
+        
+        hxyn = e_patient[H_select].values
+        
+        history = ''
+        
+        for j, y in enumerate(hxyn):
+            if y =='Yes' or y =='YES' or y =='yes':
+                history += H_content[j]
+                
+        hx_token_ids = []
+        hx_token_ids.append(torch.tensor([101,0,0,0,0],dtype=torch.float32)) 
+               
+        
+        hx_tokens = self.tokanizer.tokenize(history)
+        # add BERT cls head
+        while len(hx_tokens)>511:
+            hx_tokens_i,hx_tokens = hx_tokens[:511],hx_tokens[511:]
+            hx_tokens_i = [self.tokanizer.cls_token, *hx_tokens_i]
+            hx_token_ids_ = self.tokanizer.convert_tokens_to_ids(hx_tokens_i)
+            hx_token_ids.append(torch.tensor(hx_token_ids_,dtype=torch.float32))
+        hx_tokens = [self.tokanizer.cls_token, *hx_tokens]
+        hx_tokens = hx_tokens[:512]
+        hx_token_ids_ = self.tokanizer.convert_tokens_to_ids(hx_tokens)
+        hx_token_ids.append(torch.tensor(hx_token_ids_,dtype=torch.float32))
+        
+
+        # ee_patient = e_patient.reshape(1,-1) 
+        #          0   1      2      3   4  5  6   7 8  9 10  11     12      13      14
+    # structure = y/o, sex, revisit,SBP,DBP,P,SPO2,R,T, H, W,Pain,Triage_C, Triage_H
+    # structure = y/o, sex, revisit,SBP,DBP,P,SPO2,R,T, H, W,Pain,   BE,     BV      BM
+        S_select = ['AGE',
+                'SEX',
+                'GCS',
+                'BPSYS',
+                'BPDIAS',
+                'PULSE',
+                'POPCT',
+                'RESPR',
+                'TEMPF',
+                'HEIGHT', 
+                'WEIGHT',
+                'PAINSCALE',
+                'BE',
+                'BV',
+                'BM'
+                ]
+        structure_df = e_patient[S_select]
+        
+        structure = np.array(structure_df,dtype='float')
+        
+        structure_attention_mask = np.ones(structure.shape)
+        structure_position_ids = np.arange(0, len(S_select))
+        
+        for i,d in enumerate(structure):
+            if str(d) == 'nan':
+                structure[i] = 999
+                structure_attention_mask[i]=0
+                    
+        nan = np.isnan(structure)
+                    
+        # # normalization
+        structure_np = np.float32(structure)
+        structure_mean = self.normalization[0]
+#        structure_mean[0] = 43.34
+        structure_mean[1] = 0.5
+        structure_mean[2] = 0
+        structure_std  = self.normalization[1]
+#        structure_std[0] = 26.97
+        structure_std[1] = 0.5
+        structure_std[2] = 1
+        structure_normalization = (structure_np-structure_mean)/(structure_std+1e-6)
+        
+        structure_normalization[nan] = 0
+        nan = structure_np == 999       
+        structure_normalization[nan] = 0
+        if structure_np[3] == 999:
+            structure_normalization[3] = -4
+        if structure_np[4] == 999:
+            structure_normalization[4] = -4
+            
+        structure_tensor = torch.tensor(np.float32(structure_normalization),dtype=torch.float32)
+        structure_attention_mask_tensor = torch.tensor(structure_attention_mask,dtype=torch.long)
+        structure_position_ids_tensor = torch.tensor(structure_position_ids,dtype=torch.long)
+        
+        chief_complaint_tensor = torch.tensor(cc_token_ids,dtype=torch.float32)
+
+        target_select = ['DOA',
+                         'DIEDED',
+                         'DOA',
+                         'ICU',
+                         'DIEDED',
+                         'AGE',
+                         'SEX',
+                         'DOA',
+                         'DOA',
+                         'query']
+        
+        trg = e_patient[target_select]
+               
+        trg_tensor = torch.tensor(trg,dtype=torch.float32)
+
+        
+        for i,d in enumerate(structure):
+            if str(d) == 'nan':
+                structure[i] = -1
+        
+        portal = {'CC':str(chief_complaint),
+                  'HX':history,
+                  'SS':structure}
+        
+        datas = {'structure': structure_tensor,
+                 'structure_attention_mask': structure_attention_mask_tensor,
+                 'structure_position_ids': structure_position_ids_tensor,
+                 'cc': chief_complaint_tensor,
+                 'hx': hx_token_ids,
+                 'trg':trg_tensor,
+                 'portal':portal,
+                 'idx':e_patient['idx']
+                 }             
+          
+        return datas
+    
+    def __len__(self):
+        return self.len
+
+def collate_fn(datas):
+    from torch.nn.utils.rnn import pad_sequence
+    batch = {}
+    structure = [DD['structure'] for DD in datas]
+    structure_attention_mask = [DD['structure_attention_mask'] for DD in datas]
+    structure_position_ids = [DD['structure_position_ids'] for DD in datas]
+    cc = [DD['cc'] for DD in datas]
+    stack_hx_ = [DD['hx'] for DD in datas]
+    trg = [DD['trg'] for DD in datas]
+    idx = [DD['idx'] for DD in datas]
+    
+    stack_hx = []
+    hx_n = []
+    for shx in stack_hx_:
+        hx_n.append(len(shx))
+        for eshx in shx:
+            stack_hx.append(eshx)
+       
+    ehx = stack_hx
+    origin_cc_length = [len(d) for d in cc]
+    origin_ehx_length = [len(d) for d in ehx]
+    # origin_pi_length = [len(d) for d in pi]
+    
+    cc = pad_sequence(cc,
+                      batch_first = True,
+                      padding_value=0)
+    ehx = pad_sequence(ehx,
+                      batch_first = True,
+                      padding_value=0)
+
+    
+    mask_padding_cc = torch.zeros(cc.shape,dtype=torch.long)
+    mask_padding_ehx = torch.zeros(ehx.shape,dtype=torch.long)
+    
+    for i,e in enumerate(origin_cc_length):
+        mask_padding_cc[i,:e] = 1
+    for i,e in enumerate(origin_ehx_length):
+        mask_padding_ehx[i,:e] = 1
+        
+    batch['structure'] = torch.stack(structure)
+    batch['structure_attention_mask'] = torch.stack(structure_attention_mask)
+    batch['structure_position_ids'] = torch.stack(structure_position_ids)
+    batch['cc'] = cc
+    batch['ehx'] = ehx
+    
+    batch['mask_cc'] = mask_padding_cc
+    batch['mask_ehx'] = mask_padding_ehx
+    
+    batch['stack_hx_n'] = torch.tensor(hx_n)
+    batch['origin_cc_length'] = torch.tensor(origin_cc_length)
+    batch['origin_ehx_length'] = torch.tensor(origin_ehx_length)
+    
+    batch['trg'] = torch.stack(trg)
+    
+    batch['idx'] = torch.tensor(idx)
+    
+    return batch
+
---
+++ old_codes/EDisease_train_nhamcs_v001.py
@@ -0,0 +1,291 @@
+import os
+import time
+import unicodedata
+import random
+import string
+import re
+import sys, pickle
+import numpy as np
+import pandas as pd
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torch.utils.data import DataLoader, Dataset
+
+from transformers import AutoConfig, AutoTokenizer, AutoModel, BertConfig, BertModel
+
+from EDisease_utils import count_parameters, save_checkpoint, load_checkpoint
+from EDisease_config import EDiseaseConfig, StructrualConfig
+import EDisease_model_v001 as ED_model
+
+import AIED_dataloader_nhamcs as dataloader
+
+try:
+    task = sys.argv[1]
+    print('*****task= ',task)
+except:
+    task = 'test_nhamcs_cls'
+
+batch_size = 8
+device = 'cuda'
+parallel = False
+
+checkpoint_file = '../checkpoint_EDs/test01'
+alpha=1
+beta=1
+gamma=0.1
+
+if not os.path.isdir(checkpoint_file):
+    os.makedirs(checkpoint_file)
+    print(f' make dir {checkpoint_file}')
+    
+def train_NHAMCS(EDisease_Model,
+                 stc2emb,
+                 emb_emb,
+                 dim_model,
+                 baseBERT,
+                 dloader,
+                 lr=1e-4,
+                 epoch=100,
+                 log_interval=10,
+                 noise_scale=0.002,
+                 mask_ratio=0.33,
+                 parallel=parallel,                     
+                 checkpoint_file=checkpoint_file,
+                 normalization=None
+                 ): 
+    
+    EDisease_Model.to(device)
+    stc2emb.to(device)
+    emb_emb.to(device)
+    dim_model.to(device)
+    baseBERT.to(device)
+    
+    baseBERT.eval()
+        
+    model_optimizer = optim.Adam(EDisease_Model.parameters(), lr=lr)
+    model_optimizer_s2e = optim.Adam(stc2emb.parameters(), lr=lr)
+    model_optimizer_e2e = optim.Adam(emb_emb.parameters(), lr=lr)
+    model_optimizer_dim = optim.Adam(dim_model.parameters(), lr=lr)
+    
+    if parallel:
+        EDisease_Model = torch.nn.DataParallel(EDisease_Model)
+        stc2emb = torch.nn.DataParallel(stc2emb)
+        emb_emb = torch.nn.DataParallel(emb_emb)
+        dim_model = torch.nn.DataParallel(dim_model)
+        baseBERT = torch.nn.DataParallel(baseBERT)
+    else:
+        if device == 'cuda':
+            torch.cuda.set_device(0)
+            
+    total_loss = []
+    
+    for ep in range(epoch):   
+        t0 = time.time()
+        epoch_loss = 0
+        epoch_cases =0
+        
+        for batch_idx, sample in enumerate(dloader):
+            model_optimizer.zero_grad()
+            model_optimizer_dim.zero_grad()
+            model_optimizer_s2e.zero_grad()
+            model_optimizer_e2e.zero_grad()
+            
+            loss = torch.tensor(0.)
+
+            sample = {k:v.to(device) for k,v in sample.items()}
+            
+            # for text data
+            c,cm,h,hm = sample['cc'],sample['mask_cc'],sample['ehx'],sample['mask_ehx']
+            output = baseBERT(c.long(),cm.long())
+            c_emb = output['heads']
+            
+            c_emb_emb = emb_emb(c_emb)
+
+            output = baseBERT(h.long(),hm.long())
+            h_emb = output['heads']
+            em_h_emb = emb_emb(h_emb)
+            
+            cumsum_hx_n = torch.cumsum(sample['stack_hx_n'],0)
+            h_emb_mean_ = []
+            for i,e in enumerate(cumsum_hx_n):            
+                if sample['stack_hx_n'][i]>1:
+                    h_mean = torch.mean(em_h_emb[1:cumsum_hx_n[i]],dim=0) if i < 1 else torch.mean(em_h_emb[1+cumsum_hx_n[i-1]:cumsum_hx_n[i]],dim=0)
+                    h_emb_mean_.append(h_mean)
+                # else:
+                #     h_emb_mean_.append(em_PAD_emb.view(em_h_emb[0].shape))
+                    
+            h_emb_emb = torch.stack(h_emb_mean_)
+            
+            # for structual data
+            s,sp, sm = sample['structure'],sample['structure_position_ids'], sample['structure_attention_mask']
+                  
+            if normalization is None:
+                s_noise = s
+            else:
+                #normalization = torch.tensor(normalization).expand(s.shape).to(self.device)
+                normalization = torch.ones(s.shape).to(device)
+                noise_ = normalization*noise_scale*torch.randn_like(s,device=device)
+                s_noise = s+noise_
+                
+            
+            s_emb = stc2emb(inputs=s_noise,
+                                 attention_mask=sm,
+                                 position_ids=sp)
+            s_emb_org = stc2emb(inputs=s,
+                                 attention_mask=sm,
+                                 position_ids=sp)
+            
+            # make EDisease input data
+            things = {'s':{'emb':s_emb,
+                           'attention_mask':torch.ones(s_emb.shape[:2],device=device),
+                           'position_id':5*torch.ones(h_emb_emb.unsqueeze(1).shape[:2],device=device)
+                           },
+                      'c':{'emb':c_emb_emb.unsqueeze(1),
+                           'attention_mask':torch.ones(c_emb_emb.unsqueeze(1).shape[:2],device=device),
+                           'position_id':6*torch.ones(c_emb_emb.unsqueeze(1).shape[:2],device=device)
+                           },
+                      'h':{'emb':h_emb_emb.unsqueeze(1),
+                           'attention_mask':torch.ones(h_emb_emb.unsqueeze(1).shape[:2],device=device),
+                           'position_id':7*torch.ones(h_emb_emb.unsqueeze(1).shape[:2],device=device)
+                           },
+                      }
+
+            outp = EDisease_Model(things,
+                                  noise_scale=noise_scale,
+                                  mask_ratio=mask_ratio
+                                  )
+            EDisease = outp['EDisease']
+            input_emb_org = outp['input_emb_org']
+            position_ids = outp['position_ids']
+            attention_mask = outp['attention_mask']
+
+            aug2 = 2*random.random()
+            outp2 = EDisease_Model(things=things,
+                                   noise_scale=aug2*noise_scale,
+                                   mask_ratio=mask_ratio
+                                   )
+            EDisease2 = outp2['EDisease']
+            EDiseaseFake = torch.cat([EDisease[1:],EDisease[:1]],dim=0)
+            
+            bs = len(sample['structure'])            
+
+            mode = 'D' if batch_idx%2==0 else 'G'
+            ptloss = True if batch_idx%99==3 else False
+            
+            things_e = {'e':{'emb':EDisease.unsqueeze(1),
+                             'emb2':EDisease2.unsqueeze(1),
+                             'embf':EDiseaseFake.unsqueeze(1),
+                             'attention_mask':torch.ones(EDisease.unsqueeze(1).shape[:2],device=device),
+                             'position_id':4*torch.ones(EDisease.unsqueeze(1).shape[:2],device=device)
+                             }
+                        }
+
+            loss_dim = dim_model(things=things,
+                                 things_e=things_e,
+                                 soft=0.7, 
+                                 mask_ratio=mask_ratio,
+                                 mode=mode, 
+                                 ptloss=ptloss,
+                                 ep=ep)
+            
+            loss = loss_dim
+                
+            loss.sum().backward()
+            model_optimizer.step()
+            model_optimizer_dim.step()
+            model_optimizer_s2e.step()
+            model_optimizer_e2e.step()
+                
+            with torch.no_grad():
+                epoch_loss += loss.item()*bs
+                epoch_cases += bs
+
+        if ep % 1 ==0:
+            save_checkpoint(checkpoint_file=checkpoint_file,
+                            checkpoint_path='EDisease_Model.pth',
+                            model=EDisease_Model,
+                            parallel=parallel)
+            save_checkpoint(checkpoint_file=checkpoint_file,
+                            checkpoint_path='dim_model.pth',
+                            model=dim_model,
+                            parallel=parallel)
+            save_checkpoint(checkpoint_file=checkpoint_file,
+                            checkpoint_path='stc2emb.pth',
+                            model=stc2emb,
+                            parallel=parallel)
+            save_checkpoint(checkpoint_file=checkpoint_file,
+                            checkpoint_path='emb_emb.pth',
+                            model=emb_emb,
+                            parallel=parallel)
+            print('======= epoch:%i ========'%ep)
+            
+        print('++ Ep Time: {:.1f} Secs ++'.format(time.time()-t0)) 
+        total_loss.append(float(epoch_loss/epoch_cases))
+        pd_total_loss = pd.DataFrame(total_loss)
+        pd_total_loss.to_csv('./loss_record/total_loss.csv', sep = ',')
+    print(total_loss) 
+
+        
+'  =======================================================================================================  '   
+'  =======================================================================================================  '   
+'  =======================================================================================================  '   
+'  =======================================================================================================  '   
+'  =======================================================================================================  '   
+            
+if task=='nhamcs_train':
+    model_name = "bert-base-multilingual-cased"
+    T_config = EDiseaseConfig()
+    S_config = StructrualConfig()
+    
+    baseBERT = ED_model.adjBERTmodel(bert_ver=model_name,T_config=T_config,fixBERT=True)
+    
+    BERT_tokenizer = AutoTokenizer.from_pretrained(model_name)
+    
+    EDisease_Model = ED_model.EDisease_Model(T_config=T_config,
+                                             S_config=S_config
+                                             )
+
+    stc2emb = ED_model.structure_emb(S_config)
+    emb_emb = ED_model.emb_emb(T_config)
+
+    dim_model = ED_model.DIM(T_config=T_config,
+                              alpha=alpha,
+                              beta=beta,
+                              gamma=gamma)
+
+# ====
+    all_datas = dataloader.load_datas()
+    datas_train = all_datas['datas_train']
+    dm_normalization_np = all_datas['dm_normalization_np']   
+    datas_test = all_datas['datas_test']
+    datas_val = all_datas['datas_val']   
+    datas_all = all_datas['datas']
+    
+    # datas_val = datas_val[datas_val['AGE']>=18]
+    
+    EDEW_DS_train = dataloader.EDEW_Dataset(ds= datas_train,          
+                                           tokanizer= BERT_tokenizer,
+                                           normalization = dm_normalization_np,)  
+    
+    EDEW_DL_train = DataLoader(dataset = EDEW_DS_train,
+                         shuffle = True,
+                         num_workers=4,
+                         batch_size=batch_size,
+                         collate_fn=dataloader.collate_fn)
+
+    train_NHAMCS(EDisease_Model=EDisease_Model,
+                 stc2emb=stc2emb,
+                 emb_emb=emb_emb,
+                 dim_model=dim_model,
+                 baseBERT=baseBERT,
+                 dloader=EDEW_DL_train,
+                 lr=1e-5,
+                 epoch=100,
+                 log_interval=10,
+                 noise_scale=0.002,
+                 mask_ratio=0.33,
+                 parallel=parallel,                     
+                 checkpoint_file=checkpoint_file,
+                 normalization=dm_normalization_np) 
