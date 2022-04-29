d4f15ea127b1ad12a7f4a9d098a16d230d1b44a5
wicebing <wicebing@gmail.com>
2022-04-29 17:26:13 +0800

    5

--- train_s_ts_ablastion.sh
+++ train_s_ts_ablastion.sh
@@ -1,10 +1,14 @@
 echo "====== train SPEC ? ? ======" $1 $2
-python EDisease_train_mimic_timesequence_ablation.py train_ $1 $2 origin mask
+python EDisease_train_mimic_timesequence_ablation.py train_ $1 1 origin mask
 
 echo "====== test SPEC ? ? EM ======" $1 $2
-python EDisease_train_mimic_timesequence_ablation.py test_ $1 $2 origin vtype
+python EDisease_train_mimic_timesequence_ablation.py test_ $1 1 origin vtype
 
+echo "====== train SPEC ? ? ======" $1 $2
+python EDisease_train_mimic_timesequence_ablation.py train_ $1 2 origin mask
 
+echo "====== test SPEC ? ? EM ======" $1 $2
+python EDisease_train_mimic_timesequence_ablation.py test_ $1 2 origin vtype
 
 
 
